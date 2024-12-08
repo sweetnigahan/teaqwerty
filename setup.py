fc1aa57cd78b852ec6e68934897fa623d7be4bbe
sweetnigahan <128050271+sweetnigahan@users.noreply.github.com>
2024-12-08 18:08:36 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="teaqwerty",
+    version="1.0.0",
+    project_urls={
+        'Source': 'https://github.com/sweetnigahan/teaqwerty'
+    },
+   
+)
